4cf090ba3c347b1110c6ca39c07aacaceead0806
zeekkhan <zeekkhanz10000@gmail.com>
2024-09-29 14:55:47 +0500

    done

--- server.mjs
+++ server.mjs
@@ -3,11 +3,13 @@
     const app = express();
     const PORT = 3000;
 
+
     // Route to handle API requests
     app.use('/api/user/', (req, res) => {
     res.status(200).json({ message: 'Server created successfully' });
     });
 
+    
     // Start the server
     app.listen(PORT, () => {
     console.log(`Server is listening on http://localhost:${PORT}`);
